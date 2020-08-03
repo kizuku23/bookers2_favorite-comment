2224d5004a8ee844c5894e142b2203c2fc6a981a
vagrant <vagrant@localhost.localdomain>
2020-08-03 15:10:51 +0000

    add like button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, except: [:new]
+  resources :books, except: [:new] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index, :show, :edit, :update]
 end
